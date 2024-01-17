6c5a26cd877e63f98523657766cf858e2db6d8f6
Nahum Shalman <nshalman@equinix.com>
2024-01-17 16:43:10 +0000

    cargo add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+#[tracing::instrument]
+fn hello_world() {
+    tracing::info!("Hello, world!");
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     std::env::set_var("OTEL_SERVICE_NAME", "basic");
     std::env::set_var("OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4317");
     init_tracing_opentelemetry::tracing_subscriber_ext::init_subscribers()
         .expect("init subscribers");
-    println!("Hello, world!");
+    hello_world();
     Ok(())
 }
