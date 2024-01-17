83cca6f55184f0e17faf71269dffbb94213d019a
Nahum Shalman <nshalman@equinix.com>
2024-01-17 16:41:06 +0000

    need more features

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    std::env::set_var("OTEL_SERVICE_NAME", "basic");
+    std::env::set_var("OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4317");
+    init_tracing_opentelemetry::tracing_subscriber_ext::init_subscribers()
+        .expect("init subscribers");
     println!("Hello, world!");
     Ok(())
 }
