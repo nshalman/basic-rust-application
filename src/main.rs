6149901c5cdbb5a2de6212a1cac22ec8b2b31d9b
Nahum Shalman <nshalman@equinix.com>
2024-01-17 16:46:01 +0000

    add opentelemetry properly

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     std::env::set_var("OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4317");
     init_tracing_opentelemetry::tracing_subscriber_ext::init_subscribers()
         .expect("init subscribers");
+
     hello_world();
+
+    opentelemetry::global::shutdown_tracer_provider();
     Ok(())
 }
