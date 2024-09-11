28ddc95b6af0553e85a470d3f3aef99f80a54ef8
Nahum Shalman <nahamu@gmail.com>
2024-09-11 13:04:47 +0000

    XXX copy over Nix environment

--- devenv.nix
+++ devenv.nix
@@ -3,12 +3,28 @@
   languages.nix.enable = true;
   languages.rust.enable = true;
 
-  # https://devenv.sh/pre-commit-hooks/
-  pre-commit.hooks.alejandra.enable = true;
-  pre-commit.hooks.rustfmt.enable = true;
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    alejandra
+    otel-cli
+    cargo-rdme
+  ];
 
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
+  pre-commit = {
+    # https://devenv.sh/pre-commit-hooks/
+    hooks = {
+      alejandra.enable = true;
+      cargo-check.enable = true;
+      clippy.enable = true;
+      clippy.settings.denyWarnings = true;
+      rustfmt.enable = true;
+      taplo.enable = true;
+    };
+  };
 
+  env = {
+    OTEL_EXPORTER_OTLP_ENDPOINT = "grpc://localhost:4317";
+    OTEL_EXPORTER_OTLP_INSECURE = "true";
+  };
   # See full reference at https://devenv.sh/reference/options/
 }
